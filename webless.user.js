1f00c038ccc1494f90b39c33b90dd6c55c2e95d5
yuki kurihara <co000ri@gmail.com>
2012-07-14 11:38:41 +0900

    add half

--- webless.user.js
+++ webless.user.js
@@ -5,22 +5,21 @@
 // @include     http://*
 // @include     https://*
 // @author      ksss
-// @version     0.2
+// @version     0.3
 // ==/UserScript==
 // lessっぽくブラウジングしたいscript
 
 (function () {
 
-var on = true;
-var keyString = function (e) {
+function keyString (e) {
 	var ret = '';
 	var code = e.which || e.keyCode;
-	var charCode = String.fromCharCode(code);
+	var string = String.fromCharCode(code);
 
 	if (e.ctrlKey) ret += 'C-';
 
-	if (/\w/.test(charCode)) {
-		ret += charCode[e.shiftKey ? 'toUpperCase' : 'toLowerCase']();
+	if (/\w/.test(string)) {
+		ret += string[e.shiftKey ? 'toUpperCase' : 'toLowerCase']();
 	} else {
 		ret += {
 			27: 'ESC',
@@ -30,17 +29,19 @@ var keyString = function (e) {
 	return ret;
 };
 
+var on = true;
 document.body.addEventListener('keydown', function (e) {
-	var fn = {
+	var count = 0;
+	var toggle = {
 		'ESC': function () {
 			on = !on;
 			return;
 		},
 		'C-[': function () {
-			fn['ESC']();
+			toggle['ESC']();
 		},
 	};
-	var fn2 = {
+	var config = {
 		'j': function () {
 			document.body.scrollTop += 30;
 		},
@@ -53,6 +54,12 @@ document.body.addEventListener('keydown', function (e) {
 		'G': function () {
 			document.body.scrollTop = document.body.scrollHeight;
 		},
+		'C-d': function () {
+			document.body.scrollTop += window.innerHeight / 2;
+		},
+		'C-u': function () {
+			document.body.scrollTop -= window.innerHeight / 2;
+		},
 		'C-f': function () {
 			document.body.scrollTop += window.innerHeight;
 		},
@@ -61,12 +68,12 @@ document.body.addEventListener('keydown', function (e) {
 		}
 	};
 	var key = keyString(e);
-	var handler = fn[key];
+	var handler = toggle[key];
 
 	if (handler) {
 		handler();
 	} else if (on) {
-		handler = fn2[key];
+		handler = config[key];
 		handler && handler();
 	}
 });
