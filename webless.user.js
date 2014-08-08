d8f1d64669abb325f676ec26dff7e77451800ca6
ksss <co000ri@gmail.com>
2014-08-08 18:08:37 +0900

    Change trigger to push with contrl key

--- webless.user.js
+++ webless.user.js
@@ -5,7 +5,7 @@
 // @include     http://*
 // @include     https://*
 // @author      ksss
-// @version     0.4
+// @version     0.5
 // ==/UserScript==
 // lessっぽくブラウジングしたいscript
 
@@ -16,7 +16,7 @@ function keyString (e) {
 	var code = e.which || e.keyCode;
 	var string = String.fromCharCode(code);
 
-	if (e.ctrlKey) key_string += 'C-';
+	if (e.ctrlKey) key_string += 'Ctrl-';
 
 	if (/\w/.test(string)) {
 		key_string += string[e.shiftKey ? 'toUpperCase' : 'toLowerCase']();
@@ -32,42 +32,42 @@ function keyString (e) {
 };
 
 var on = true;
+var toggle = {
+	'ESC': function () {
+		on = !on;
+	},
+	'Ctrl-[': function () {
+		toggle['ESC']();
+	},
+};
+var config = {
+	'Ctrl-j': function () {
+		document.body.scrollTop += 30;
+	},
+	'Ctrl-k': function () {
+		document.body.scrollTop -= 30;
+	},
+	'Ctrl-g': function () {
+		document.body.scrollTop = 0;
+	},
+	'Ctrl-G': function () {
+		document.body.scrollTop = document.body.scrollHeight;
+	},
+	'Ctrl-d': function () {
+		document.body.scrollTop += window.innerHeight / 2;
+	},
+	'Ctrl-u': function () {
+		document.body.scrollTop -= window.innerHeight / 2;
+	},
+	'Ctrl-f': function () {
+		document.body.scrollTop += window.innerHeight;
+	},
+	'Ctrl-b': function () {
+		document.body.scrollTop -= window.innerHeight;
+	}
+};
 document.body.addEventListener('keydown', function (e) {
 	var count = 0;
-	var toggle = {
-		'ESC': function () {
-			on = !on;
-		},
-		'C-[': function () {
-			toggle['ESC']();
-		},
-	};
-	var config = {
-		'j': function () {
-			document.body.scrollTop += 30;
-		},
-		'k': function () {
-			document.body.scrollTop -= 30;
-		},
-		'g': function () {
-			document.body.scrollTop = 0;
-		},
-		'G': function () {
-			document.body.scrollTop = document.body.scrollHeight;
-		},
-		'd': function () {
-			document.body.scrollTop += window.innerHeight / 2;
-		},
-		'u': function () {
-			document.body.scrollTop -= window.innerHeight / 2;
-		},
-		'f': function () {
-			document.body.scrollTop += window.innerHeight;
-		},
-		'b': function () {
-			document.body.scrollTop -= window.innerHeight;
-		}
-	};
 	var key = keyString(e);
 	var handler = toggle[key];
 
